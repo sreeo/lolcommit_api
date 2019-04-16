57db350a08edca61fad4459c58de127457ee5cc8
Sreeraj <rajan.sreeraj@yahoo.com>
2019-04-16 13:39:12 +0530

    Add url in commit

--- priv/repo/migrations/20190416080037_add_url_in_commits.exs
+++ priv/repo/migrations/20190416080037_add_url_in_commits.exs
@@ -2,6 +2,8 @@ defmodule LolcommitServer.Repo.Migrations.AddUrlInCommits do
   use Ecto.Migration
 
   def change do
-
+    alter table(:commits) do
+      add :url, :string # Database type
+    end
   end
 end
